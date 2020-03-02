1d854b91c43346bb92252108c77ca5321667ade5
Pavlos Karakalidis <pavloc.kara@outlook.com>
2020-03-02 03:12:09 +0200

    Add authentication check to tag model

--- server/resolvers/tag.js
+++ server/resolvers/tag.js
@@ -1,5 +1,6 @@
 const {TagModel} = require('../models/tag');
 const {ObjectId} = require('mongodb').ObjectId;
+const {ForbiddenError} = require('apollo-server-express');
 
 module.exports.tag = async(_, args, req) => {
     try {
@@ -18,52 +19,70 @@ module.exports.tags = async (_, args, req) => {
 };
 
 module.exports.addTag = async(_,args, req) => {
-    let lastTag = await TagModel.find({}).sort({_id: -1}).limit(1);
-    let tags = await TagModel.find({name: args.tag.name});
-    if(Array.isArray(tags) && tags.length > 0){
-        throw new Error("A tag with this name already exists");
-    }
-    if(Array.isArray(lastTag) && lastTag.length > 0){
-        lastTag = lastTag[0]
-    }
-    const tag = new TagModel({
-        _id: lastTag._id + 1,
-        name: args.tag.name
-    });
-    if(Array.isArray(lastTag) && lastTag.length === 0){
-        tag._id = 0;
-    }
-    try {
-        return await tag.save();
-    } catch (e) {
-        console.log('Could not save the tag');
-        console.log(e);
+    req.user = {...req.user, ...(await req.user.checkAuthentication())};
+    const reqUser = req.user._doc;
+    if(reqUser && reqUser.accountType && reqUser.accountType === 'admin') {
+        let lastTag = await TagModel.find({}).sort({_id: -1}).limit(1);
+        let tags = await TagModel.find({name: args.tag.name});
+        if (Array.isArray(tags) && tags.length > 0) {
+            throw new Error("A tag with this name already exists");
+        }
+        if (Array.isArray(lastTag) && lastTag.length > 0) {
+            lastTag = lastTag[0]
+        }
+        const tag = new TagModel({
+            _id: lastTag._id + 1,
+            name: args.tag.name
+        });
+        if (Array.isArray(lastTag) && lastTag.length === 0) {
+            tag._id = 0;
+        }
+        try {
+            return await tag.save();
+        } catch (e) {
+            console.log('Could not save the tag');
+            console.log(e);
+        }
+    } else {
+        throw new ForbiddenError('403-Forbidden');
     }
 };
 
 module.exports.editTag = async (_, args, req) => {
-  const tag = await TagModel.findOne({_id: args.tag._id});
-  if(tag){
-      try {
-          return await TagModel.findOneAndUpdate({_id: args.tag._id}, {$set: args.tag}, {new: true});
-      } catch (e){
-          console.log(e);
-          throw e;
-      }
-  }
+    req.user = {...req.user, ...(await req.user.checkAuthentication())};
+    const reqUser = req.user._doc;
+    if(reqUser && reqUser.accountType && reqUser.accountType === 'admin') {
+        const tag = await TagModel.findOne({_id: args.tag._id});
+        if (tag) {
+            try {
+                return await TagModel.findOneAndUpdate({_id: args.tag._id}, {$set: args.tag}, {new: true});
+            } catch (e) {
+                console.log(e);
+                throw e;
+            }
+        }
+    } else {
+        throw new ForbiddenError('403-Forbidden');
+    }
 };
 
 module.exports.deleteTag = async (_, args, req) => {
-  const tag = await TagModel.findById(args.tag._id);
-      if(tag){
-          if(tag.name !== args.tag.name){
-              throw new Error("The name of tags doesn't match");
-          }
-          try {
-              return await TagModel.findByIdAndDelete(args.tag._id);
-          } catch (e) {
-              console.log(e);
-              throw e;
-          }
-      }
+    req.user = {...req.user, ...(await req.user.checkAuthentication())};
+    const reqUser = req.user._doc;
+    if(reqUser && reqUser.accountType && reqUser.accountType === 'admin') {
+        const tag = await TagModel.findById(args.tag._id);
+        if (tag) {
+            if (tag.name !== args.tag.name) {
+                throw new Error("The name of tags doesn't match");
+            }
+            try {
+                return await TagModel.findByIdAndDelete(args.tag._id);
+            } catch (e) {
+                console.log(e);
+                throw e;
+            }
+        }
+    } else {
+        throw new ForbiddenError('403-Forbidden');
+    }
 };
